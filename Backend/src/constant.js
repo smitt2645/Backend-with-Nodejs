c633a6cc9b04497fff3b86335e955d81ba2a4f20
Smit Trivedi <smit.iihglobal@gmail.com>
2025-04-07 15:18:12 +0530

    removed connection string !

--- Backend/src/constant.js
+++ Backend/src/constant.js
@@ -1,3 +1,2 @@
 export const database_name = "Test"
-//MongoDB_URI : mongodb+srv://smitt533:4jU2ALi5nNMB1K0z@cluster0.h3ro0pc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 // PORT = 8002
